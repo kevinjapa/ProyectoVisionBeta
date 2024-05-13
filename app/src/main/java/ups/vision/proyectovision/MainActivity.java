0e64a7db95399b86cf025730c3e6f482a9f5b87f
kevinjapa <84851902+kevinjapa@users.noreply.github.com>
2024-05-13 01:29:03 -0500

    update camera
    ya funca la camara para abrir desde open cv

--- app/src/main/java/ups/vision/proyectovision/MainActivity.java
+++ app/src/main/java/ups/vision/proyectovision/MainActivity.java
@@ -1,75 +1,90 @@
 package ups.vision.proyectovision;
 
-import androidx.appcompat.app.AppCompatActivity;
+
+import org.opencv.android.CameraActivity;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Mat;
+
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.SurfaceView;
+import android.Manifest;
 
-public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-    private CameraBridgeViewBase mOpenCvCameraView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.security.auth.Destroyable;
+
+public class MainActivity extends CameraActivity {
+
+    CameraBridgeViewBase cameraBridgeViewBase;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (!OpenCVLoader.initDebug()) {
-            System.out.println("carga");
-            Log.d("LOADED", "SUCCESS");
-        } else {
-            Log.d("LOADED", "error");
-            System.out.println("no carga");
-            setupCameraView();
+
+        getPermission();
+
+        cameraBridgeViewBase = findViewById(R.id.cameraView);
+
+        cameraBridgeViewBase.setCvCameraViewListener(new CameraBridgeViewBase.CvCameraViewListener2(){
+            @Override
+            public void onCameraViewStarted(int width, int heigt){}
+            @Override
+            public void onCameraViewStopped() {}
+            @Override
+            public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame){
+                return inputFrame.rgba();
+            }
+        });
+
+        if(OpenCVLoader.initDebug()) {
+            cameraBridgeViewBase.enableView();
         }
-
-    }
-
-    private void setupCameraView() {
-        mOpenCvCameraView = findViewById(R.id.camera_view);
-        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-        mOpenCvCameraView.setCvCameraViewListener(this);
-        mOpenCvCameraView.enableView();
     }
 
     @Override
-    protected void onResume() {
+    protected void onResume(){
         super.onResume();
-        if (mOpenCvCameraView != null) {
-            mOpenCvCameraView.enableView();
-        }
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (mOpenCvCameraView != null) {
-            mOpenCvCameraView.disableView();
-        }
+    cameraBridgeViewBase.enableView();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (mOpenCvCameraView != null) {
-            mOpenCvCameraView.disableView();
+        cameraBridgeViewBase.disableView();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        cameraBridgeViewBase.disableView();
+    }
+
+
+    @Override
+    protected List<? extends CameraBridgeViewBase> getCameraViewList(){
+        return Collections.singletonList(cameraBridgeViewBase);
+    }
+
+    void getPermission(){
+        if(checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 101);
         }
     }
 
     @Override
-    public void onCameraViewStarted(int width, int height) {}
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        super.onRequestPermissionsResult(requestCode,permissions, grantResults);
+        if(grantResults.length>0 && grantResults[0]!= PackageManager.PERMISSION_GRANTED){
 
-    @Override
-    public void onCameraViewStopped() {}
-
-    @Override
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        Mat gray = inputFrame.gray();
-        Camara(gray.getNativeObjAddr());
-        return gray;
+        }
     }
 
-    public native void Camara(long matAddrGray);
 }
