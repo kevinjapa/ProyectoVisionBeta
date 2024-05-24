88533ba550e550376210210825817e89ef7c2a28
kevinjapa <84851902+kevinjapa@users.noreply.github.com>
2024-05-23 21:13:26 -0500

    update
    actualizacion de componente y funcionalidad de envio por socket y filtro de fondo verde

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -21,30 +21,98 @@ Java_ups_vision_proyectovision_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
-
-
 //extern "C" JNIEXPORT void JNICALL
 //Java_ups_vision_proyectovision_MainActivity_imagenGris(JNIEnv *env, jobject object, jobject imagen) {
 //   Mat mGray= reinterpret_cast<Mat &&>(imagen);
 //   cvtColor(mGray, mGray, COLOR_RGBA2GRAY);
 //}
-//extern "C"
-//JNIEXPORT void JNICALL
-//Java_ups_vision_proyectovision_MainActivity_imagenGris(JNIEnv *env, jobject instance, jobject bitmap) {
-//    AndroidBitmapInfo info;
-//    void* pixels;
-//
-//
-//    // Crea un objeto cv::Mat desde el bitmap
-//    Mat src(info.height, info.width, CV_8UC4, pixels);
-//    Mat gray;
-//
-//    // Convierte a escala de grises
-//    cvtColor(src, gray, COLOR_RGBA2GRAY);
-//    cvtColor(gray, src, COLOR_GRAY2RGBA);
-//
-//    AndroidBitmap_unlockPixels(env, bitmap);
-//}
+
+void bitmapToMat(JNIEnv * env, jobject bitmap, cv::Mat &dst, jboolean needUnPremultiplyAlpha){
+    AndroidBitmapInfo info;
+    void* pixels = 0;
+    try {
+        CV_Assert( AndroidBitmap_getInfo(env, bitmap, &info) >= 0 );
+        CV_Assert( info.format == ANDROID_BITMAP_FORMAT_RGBA_8888 ||
+                   info.format == ANDROID_BITMAP_FORMAT_RGB_565 );
+        CV_Assert( AndroidBitmap_lockPixels(env, bitmap, &pixels) >= 0 );
+        CV_Assert( pixels );
+        dst.create(info.height, info.width, CV_8UC4);
+        if( info.format == ANDROID_BITMAP_FORMAT_RGBA_8888 )
+        {
+            cv::Mat tmp(info.height, info.width, CV_8UC4, pixels);
+            if(needUnPremultiplyAlpha) cvtColor(tmp, dst, cv::COLOR_mRGBA2RGBA);
+            else tmp.copyTo(dst);
+        } else {
+            // info.format == ANDROID_BITMAP_FORMAT_RGB_565
+            cv::Mat tmp(info.height, info.width, CV_8UC2, pixels);
+            cvtColor(tmp, dst, cv::COLOR_BGR5652RGBA);
+        }
+        AndroidBitmap_unlockPixels(env, bitmap);
+        return;
+    } catch(const cv::Exception& e) {
+        AndroidBitmap_unlockPixels(env, bitmap);
+        //jclass je = env->FindClass("org/opencv/core/CvException");
+        jclass je = env->FindClass("java/lang/Exception");
+        //if(!je) je = env->FindClass("java/lang/Exception");
+        env->ThrowNew(je, e.what());
+        return;
+    } catch (...) {
+        AndroidBitmap_unlockPixels(env, bitmap);
+        jclass je = env->FindClass("java/lang/Exception");
+        env->ThrowNew(je, "Unknown exception in JNI code {nBitmapToMat}");
+        return;
+    }
+}
+
+void matToBitmap(JNIEnv * env, cv::Mat src, jobject bitmap, jboolean needPremultiplyAlpha) {
+    AndroidBitmapInfo info;
+    void *pixels = 0;
+    try {
+        CV_Assert(AndroidBitmap_getInfo(env, bitmap, &info) >= 0);
+        CV_Assert(info.format == ANDROID_BITMAP_FORMAT_RGBA_8888 ||
+                  info.format == ANDROID_BITMAP_FORMAT_RGB_565);
+        CV_Assert(src.dims == 2 && info.height == (uint32_t) src.rows &&
+                  info.width == (uint32_t) src.cols);
+        CV_Assert(src.type() == CV_8UC1 || src.type() == CV_8UC3 || src.type() == CV_8UC4);
+        CV_Assert(AndroidBitmap_lockPixels(env, bitmap, &pixels) >= 0);
+        CV_Assert(pixels);
+        if (info.format == ANDROID_BITMAP_FORMAT_RGBA_8888) {
+            cv::Mat tmp(info.height, info.width, CV_8UC4, pixels);
+            if (src.type() == CV_8UC1) {
+                cvtColor(src, tmp, cv::COLOR_GRAY2RGBA);
+            } else if (src.type() == CV_8UC3) {
+                cvtColor(src, tmp, cv::COLOR_RGB2RGBA);
+            } else if (src.type() == CV_8UC4) {
+                if (needPremultiplyAlpha) cvtColor(src, tmp, cv::COLOR_RGBA2mRGBA);
+                else src.copyTo(tmp);
+            }
+        } else {
+            // info.format == ANDROID_BITMAP_FORMAT_RGB_565
+            cv::Mat tmp(info.height, info.width, CV_8UC2, pixels);
+            if (src.type() == CV_8UC1) {
+                cvtColor(src, tmp, cv::COLOR_GRAY2BGR565);
+            } else if (src.type() == CV_8UC3) {
+                cvtColor(src, tmp, cv::COLOR_RGB2BGR565);
+            } else if (src.type() == CV_8UC4) {
+                cvtColor(src, tmp, cv::COLOR_RGBA2BGR565);
+            }
+        }
+        AndroidBitmap_unlockPixels(env, bitmap);
+        return;
+    } catch (const cv::Exception &e) {
+        AndroidBitmap_unlockPixels(env, bitmap);
+        //jclass je = env->FindClass("org/opencv/core/CvException");
+        jclass je = env->FindClass("java/lang/Exception");
+        //if(!je) je = env->FindClass("java/lang/Exception");
+        env->ThrowNew(je, e.what());
+        return;
+    } catch (...) {
+        AndroidBitmap_unlockPixels(env, bitmap);
+        jclass je = env->FindClass("java/lang/Exception");
+        env->ThrowNew(je, "Unknown exception in JNI code {nMatToBitmap}");
+        return;
+    }
+}
 
 extern "C"
 JNIEXPORT void JNICALL
@@ -58,13 +126,48 @@ Java_ups_vision_proyectovision_MainActivity_imagenGris(JNIEnv *env, jobject inst
     if (AndroidBitmap_lockPixels(env, bitmap, &pixels) < 0) return;
 
     // Crea un objeto cv::Mat desde el bitmap
-    Mat src(info.height, info.width, CV_8UC4, pixels);
+    Mat frame(info.height, info.width, CV_8UC4, pixels);
     Mat gray;
-
     // Convierte a escala de grises
-    cvtColor(src, gray, COLOR_RGBA2GRAY);
-    cvtColor(gray, src, COLOR_GRAY2RGBA);
+    cvtColor(frame, gray, COLOR_RGBA2GRAY);
+    cvtColor(gray, frame, COLOR_GRAY2RGBA);
 
     AndroidBitmap_unlockPixels(env, bitmap);
+}
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_ups_vision_proyectovision_MainActivity_detectorBordes
+        (JNIEnv* env,
+        jobject /*this*/,
+        jobject bitmapIn,
+        jobject bitmapOut){
+    Mat src;
+
+    bitmapToMat(env, bitmapIn, src, false);
+
+    // Convertir la imagen a HSV para trabajar con el canal de color verde
+    Mat hsv;
+    cvtColor(src, hsv, COLOR_BGR2HSV);
+
+    // Definir los límites del color verde en HSV
+    Scalar verde_bajo(35, 50, 50);
+    Scalar verde_alto(85, 255, 255);
+
+    // Crear una máscara para el rango de color verde
+    Mat mascara_verde;
+    inRange(hsv, verde_bajo, verde_alto, mascara_verde);
+
+    // Invertir la máscara para obtener el fondo
+    Mat mascara_fondo;
+    bitwise_not(mascara_verde, mascara_fondo);
+
+    // Aplicar la máscara al fondo
+    Mat fondo;
+    fondo.setTo(Scalar(255, 255, 255)); // Fondo blanco
+    src.copyTo(fondo, mascara_fondo);
+
+    matToBitmap(env, fondo, bitmapOut, false);
+
 }
 
--- app/src/main/java/ups/vision/proyectovision/MainActivity.java
+++ app/src/main/java/ups/vision/proyectovision/MainActivity.java
@@ -13,11 +13,17 @@ import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView imagenTomada, imagenFiltro;
-    Bitmap bitmapOriginal;
+    Bitmap bitmapOriginal, outputBitmap;
+
     static {
         System.loadLibrary("proyectovision");
     }
@@ -30,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
 
         Button captura = findViewById(R.id.camara_main);
         Button btnFiltro = findViewById(R.id.btnFiltro);
+        Button btnBorde = findViewById(R.id.btnBorde);
+        Button btnEnviar = findViewById(R.id.btnEnviar);
+
         imagenTomada= findViewById(R.id.imgOriginal);
         imagenFiltro=findViewById(R.id.imgFiltro);
         captura.setOnClickListener(new View.OnClickListener() {
@@ -50,6 +59,48 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        btnBorde.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bitmapOriginal != null) {
+                    outputBitmap = bitmapOriginal.copy(bitmapOriginal.getConfig(), true);
+                    detectorBordes(bitmapOriginal, outputBitmap);
+                    System.out.println("boton borde");
+                    imagenFiltro.setImageBitmap(outputBitmap);
+                } else {
+                    Toast.makeText(MainActivity.this, "No image to filter", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        btnEnviar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+
+                // Envía la imagen transformada al servidor Flask
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                outputBitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                byte[] byteArray = outputStream.toByteArray();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            Socket socket = new Socket("192.168.0.114", 3500);
+                            OutputStream out = socket.getOutputStream();
+                            out.write(byteArray);
+                            out.flush();
+                            out.close();
+                            socket.close();
+                            System.out.println("Imagen Enviada");
+                        }
+                        catch (IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+            }
+        });
+
         // Recibir y decodificar el byte array
         Intent intent = getIntent();
         byte[] byteArray = intent.getByteArrayExtra("capturedImage");
@@ -66,4 +117,5 @@ public class MainActivity extends AppCompatActivity {
     }
 
     native void imagenGris(Bitmap bitmap);
+    private native void detectorBordes(android.graphics.Bitmap in, android.graphics.Bitmap out);
 }
