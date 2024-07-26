240679a69f5ca72bce86a868ce98a21d506e9662
kevinjapa <84851902+kevinjapa@users.noreply.github.com>
2024-07-25 21:50:37 -0500

    update hug

--- app/src/main/java/ups/vision/proyectovision/SecondActivity.java
+++ app/src/main/java/ups/vision/proyectovision/SecondActivity.java
@@ -9,8 +9,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class SecondActivity extends AppCompatActivity {
     Bitmap bitmapOriginal, outputBitmap;
@@ -29,6 +35,7 @@ public class SecondActivity extends AppCompatActivity {
         Button btnCamara = findViewById(R.id.btnCamara2);
         Button btnParte1 = findViewById(R.id.btnParte1);
         Button btnReconocer = findViewById(R.id.btnReconocer);
+        Button btnHug = findViewById(R.id.btnHog);
         initAssetManager(getAssets());
 //        AssetManager assetManager = getAssets();
 //        String cacheDir = getCacheDir().getAbsolutePath();
@@ -57,6 +64,46 @@ public class SecondActivity extends AppCompatActivity {
                 reconocimiento(bitmapOriginal,outputBitmap);
                 verImgOutput.setImageBitmap(outputBitmap);
                 System.out.println("Si esta entrando en la parte ----------------------------");
+            }
+        });
+        btnHug.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verImgOutput.setImageBitmap(bitmapOriginal);
+//                outputBitmap = bitmapOriginal.copy(bitmapOriginal.getConfig(), true);
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                bitmapOriginal.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                byte[] byteArray = outputStream.toByteArray();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+//                                    Socket socket = new Socket("192.168.0.112", 3500);
+                            Socket socket = new Socket("192.168.0.111", 3500);
+                            OutputStream out = socket.getOutputStream();
+                            out.write(byteArray);
+                            out.flush();
+                            out.close();
+                            socket.close();
+                            System.out.println("Imagen Enviada");
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(), "Imagen Enviado", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                        catch (IOException e){
+                            e.printStackTrace();
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }
+                }).start();
             }
         });
         Intent intent = getIntent();
